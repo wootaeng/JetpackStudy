c961d0b0cd660ddf4fc22318845ed0f3e23f2e91
wootaeng <karufuci@gmail.com>
2021-12-27 18:46:00 +0900

    navigation practic

---
+++ Navigation/NaviDemo1/app/src/main/java/com/ws/skelton/navdemo1/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.ws.skelton.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.ws.skelton.navdemo1.databinding.FragmentHomeBinding
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
+        binding.button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
+        }
+        return binding.root
+    }
+
+
+}
---
+++ Navigation/NaviDemo1/app/src/main/java/com/ws/skelton/navdemo1/SecondFragment.kt
@@ -0,0 +1,34 @@
+package com.ws.skelton.navdemo1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.ws.skelton.navdemo1.databinding.FragmentSecondBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SecondFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SecondFragment : Fragment() {
+
+
+
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_second, container, false )
+        return binding.root
+    }
+
+
+}
